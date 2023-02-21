3e04ee8b14ca8a8473ba7d1229ab9f6bb902c98b
DerekHsiehDev <devderekhsieh@gmail.com>
2023-02-21 10:08:22 -0800

    Custom LazyVim setup with GitHub CoPilot

---
+++ init.lua
@@ -0,0 +1,23 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+require("copilot").setup({
+  panel = {
+    auto_refresh = false,
+    keymap = {
+      accept = "<CR>",
+      jump_prev = "[[",
+      jump_next = "]]",
+      refresh = "gr",
+      open = "<M-CR>",
+    },
+  },
+  suggestion = {
+    auto_trigger = true,
+    keymap = {
+      accept = "<C-l>",
+      prev = "<C-[>",
+      next = "C-]>",
+      dismiss = "<C-]>",
+    },
+  },
+})
---
+++ lua/config/options.lua
@@ -0,0 +1,13 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+--
+vim.keymap.set("i", "<Tab>", function()
+  if require("copilot.suggestion").is_visible() then
+    require("copilot.suggestion").accept()
+  else
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+  end
+end, {
+  silent = true,
+})
